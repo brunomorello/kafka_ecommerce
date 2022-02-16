7cb3ff5057b36c8b0c159b85427657c20603dbf8
Bruno Moreno <brunomorello7@gmail.com>
2022-02-16 19:44:56 -0300

    Created 2 new Service Consumers: Email and Log

--- src/main/java/br/com/bmo/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/bmo/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.bmo.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -14,14 +15,19 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(getProperties());
         var value = "3312,3212,102";
         var record = new ProducerRecord<>("STORE_NEW_ORDER", value, value);
-        producer.send(record, (metadata, exception) -> {
+        Callback callback = (metadata, exception) -> {
             if (exception != null) {
                 exception.printStackTrace();
                 return;
             }
 
             System.out.println("success!\n\n" + metadata.topic() + " > partition: " + metadata.partition() + " offset: " + metadata.offset() + " timestamp: " + metadata.timestamp());
-        }).get();
+        };
+        producer.send(record, callback).get();
+
+        String emailContent = "Thanks for buying with us! Your Order is being processed";
+        ProducerRecord<String, String> emailRecord = new ProducerRecord<>("STORE_SEND_EMAIL", emailContent, emailContent);
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties getProperties() {
