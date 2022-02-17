c09b5fbb115f3b7630d4fc5dbf2ba07feff5ccbf
Bruno Moreno <brunomorello7@gmail.com>
2022-02-16 23:12:14 -0300

    Created a custom serializer (Gson) using generics

--- src/main/java/br/com/bmo/ecommerce/KafkaDispatcher.java
+++ src/main/java/br/com/bmo/ecommerce/KafkaDispatcher.java
@@ -1,5 +1,6 @@
 package br.com.bmo.ecommerce;
 
+import br.com.bmo.ecommerce.serializer.GsonSerializer;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -10,8 +11,8 @@ import java.io.Closeable;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-public class KafkaDispatcher implements Closeable {
-    private final KafkaProducer<String, String> producer;
+public class KafkaDispatcher<T> implements Closeable {
+    private final KafkaProducer<String, T> producer;
 
     KafkaDispatcher() {
         this.producer = new KafkaProducer<>(getProperties());
@@ -21,11 +22,11 @@ public class KafkaDispatcher implements Closeable {
         var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
         return properties;
     }
 
-    public void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
+    public void send(String topic, String key, T value) throws ExecutionException, InterruptedException {
         var record = new ProducerRecord<>(topic, key, value);
         Callback callback = (metadata, exception) -> {
             if (exception != null) {
--- src/main/java/br/com/bmo/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/bmo/ecommerce/NewOrderMain.java
@@ -1,18 +1,27 @@
 package br.com.bmo.ecommerce;
 
+import br.com.bmo.ecommerce.model.Order;
+
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (KafkaDispatcher dispatcher = new KafkaDispatcher()) {
-            for (int i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = "3312,3212,102";
-                dispatcher.send("STORE_NEW_ORDER", key, value);
+        try (KafkaDispatcher orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (KafkaDispatcher emailDispatcher = new KafkaDispatcher<String>()) {
+                for (int i = 0; i < 10; i++) {
 
-                var emailContent = "Thanks for buying with us! Your Order is being processed";
-                dispatcher.send("STORE_SEND_EMAIL", key, emailContent);
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("STORE_NEW_ORDER", userId, order);
+
+                    var emailContent = "Thanks for buying with us! Your Order is being processed";
+                    emailDispatcher.send("STORE_SEND_EMAIL", userId, emailContent);
+                }
             }
         }
     }
